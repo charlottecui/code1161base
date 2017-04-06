1d37606687518223b3a66df336a2d4778bdee71c
charlottecui <charlottecui1999@hotmail.com>
2017-04-06 15:05:23 +1000

    submit week3 hw

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
 """Week 3.
-
 Modify each function until the tests pass.
 """
 from __future__ import division
@@ -9,67 +8,108 @@ from __future__ import print_function
 
 def loop_ranger(start, stop=None, step=1):
     """Return a list of numbers between start and stop in steps of step.
-
     Do this using any method apart from just using range()
     """
+    first_loop = []
+    while start < stop:
+        first_loop.append(start)
+        start += step
+    return first_loop
 
 
 def lone_ranger(start, stop, step):
     """Duplicate the functionality of range.
-
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    pass
+    second_loop = []
+    for i in range(start, stop, step):
+        second_loop.append(i)
+    return second_loop
 
 
 def two_step_ranger(start, stop):
     """Make a range that steps by 2.
-
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    pass
+    step = 2
+    third_loop = []
+    while start < stop:
+        third_loop.append(start)
+        start += step
+    return third_loop
 
 
 def gene_krupa_range(start, stop, even_step, odd_step):
     """Make a range that has two step sizes.
-
     make a list that instead of having evenly spaced steps
     has odd steps be one size and even steps be another.
     """
-    pass
+    fourth_loop = []
+
+    latest = start
+    beat = 0
+
+    while latest < stop:
+        fourth_loop.append(latest)
+        if beat % 2 == 0:
+            latest += even_step
+        else:
+            latest += odd_step
+        beat += 1
+    return fourth_loop
 
 
 def stubborn_asker(low, high):
     """Ask for a number between low and high until actually given one.
-
     Ask for a number, and if the response is outside the bounds keep asking
     until you get a number that you think is OK
     """
-
-    message = "Give me a number between {low}, and {high}:". format(low=low,
-                                                                    high=high)
-    input_number = int(raw_input(message))
-    print(low < input_number, input_number)
+    message = "Give a number {low}, and {high}:".format(low=low, high=high)
+    while True:
+        input_number = int(raw_input(message))
+        if low < input_number < high:
+            print ("good job! {}".format(input_number))
+            return input_number
+        else:
+            print("{input} isn't between {low} and {high}".
+                  format(input=input_number, low=low, high=high))
 
 
 def not_number_rejector(message):
     """Ask for a number repeatedly until actually given one.
-
     Ask for a number, and if the response is actually NOT a number (e.g. "cow",
     "six", "8!") then throw it out and ask for an actual number.
     When you do get a number, return it.
     """
-    pass
+    passed = False
+    while passed is False:
+        passed = True
+        try:
+            inputted = int(str(raw_input(message)))
+        except ValueError:
+            passed = False
+    return inputted
 
 
 def super_asker(low, high):
     """Robust asking function.
-
     Combine stubborn_asker and not_number_rejector to make a function
     that does it all!
     """
-    pass
+    passed = False
+    inputted = "placeholder"
+    while passed is False:
+        passed = True
+        try:
+            inputted = int(str(raw_input()))
+        except ValueError:
+            passed = False
+        if low < inputted < high:
+            passed = True
+        else:
+            passed = False
+    return inputted
 
 
 if __name__ == "__main__":
