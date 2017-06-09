f09c9a8bd82b7d4ecf170d5e4f6a5551eb123f25
charlottecui <charlottecui1999@hotmail.com>
2017-06-09 17:05:55 +1000

    update

--- new_exercise_getter.py
+++ new_exercise_getter.py
@@ -1,29 +1,31 @@
 # -*- coding: UTF-8 -*-
-
 """Gets updated version of exercises.
+
 Checks for existing version and won't overwrite.
-
 """
-
 import os
 import requests
+
 LOCAL = os.path.dirname(os.path.realpath(__file__))
 
 
 def get_the_updates():
-
     """Decide if the other functions should download each file."""
     base = ("https://raw.githubusercontent.com/"
             "notionparallax/code1161base/master")
     new_files = [
+        "/week2/exercise0.py",
+        "/week4/IOexamples.py",
         "/week5/exercise1.py",
-        "/week5/tests.py"
+        "/week5/exercise2.py"
+        ""
     ]
 
     for f in new_files:
         save_path = "./" + f
         if not os.path.isfile(save_path) and f is not "":
             url = base + f
+            print("downloading", url)
             download_and_save(url, save_path)
         elif f is "":
             pass  # do nothing, it's padding
