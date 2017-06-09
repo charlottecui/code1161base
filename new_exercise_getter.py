7965699987661d1a3b0dcc53e4a0c81c07147cfb
charlottecui <charlottecui1999@hotmail.com>
2017-06-09 16:24:44 +1000

    update

--- new_exercise_getter.py
+++ new_exercise_getter.py
@@ -1,24 +1,23 @@
 # -*- coding: UTF-8 -*-
+
 """Gets updated version of exercises.
 Checks for existing version and won't overwrite.
+
 """
+
 import os
 import requests
-
 LOCAL = os.path.dirname(os.path.realpath(__file__))
 
 
 def get_the_updates():
+
     """Decide if the other functions should download each file."""
     base = ("https://raw.githubusercontent.com/"
             "notionparallax/code1161base/master")
     new_files = [
-        # "/week2/exercise0.py",
-        # "/week4/IOexamples.py",
-        "/week8/exercise1.py",
-        "/week8/tests.py",
-        # "/week5/exercise4.py",
-        ""
+        "/week5/exercise1.py",
+        "/week5/tests.py"
     ]
 
     for f in new_files:
